03eed2286d53f83d1b4267bc38a5096750e0845e
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2020-09-30 08:26:52 +0200

    First Commit

---
+++ MultiForm/MultiForm/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2(); // dichiarazione form2
+            f2.Show(); // visualizza form
+
+        }
+
+        private void btnForm2B_Click(object sender, EventArgs e)
+        {
+            Form f2b = new Form(); // senza utilizzare classi
+            f2b.Text = "Aperta Form Dinamica";
+            f2b.Show(); 
+        }
+    }
+}
