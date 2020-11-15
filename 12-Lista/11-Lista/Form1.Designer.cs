024387874406309d08835811abfdbc0f8d9408be
Ivan Angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2020-11-15 19:03:21 +0100

    Aggiunti Esercizi 12 e 13

---
+++ 12-Lista/11-Lista/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _11_Lista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitolo = new System.Windows.Forms.TextBox();
+            this.txtAutore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAggiungi = new System.Windows.Forms.Button();
+            this.btnVisualizza = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Titolo";
+            // 
+            // txtTitolo
+            // 
+            this.txtTitolo.Location = new System.Drawing.Point(100, 12);
+            this.txtTitolo.Name = "txtTitolo";
+            this.txtTitolo.Size = new System.Drawing.Size(100, 22);
+            this.txtTitolo.TabIndex = 1;
+            // 
+            // txtAutore
+            // 
+            this.txtAutore.Location = new System.Drawing.Point(100, 40);
+            this.txtAutore.Name = "txtAutore";
+            this.txtAutore.Size = new System.Drawing.Size(100, 22);
+            this.txtAutore.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Autore";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(100, 68);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ID";
+            // 
+            // btnAggiungi
+            // 
+            this.btnAggiungi.Location = new System.Drawing.Point(20, 96);
+            this.btnAggiungi.Name = "btnAggiungi";
+            this.btnAggiungi.Size = new System.Drawing.Size(84, 49);
+            this.btnAggiungi.TabIndex = 6;
+            this.btnAggiungi.Text = "Aggiungi libro";
+            this.btnAggiungi.UseVisualStyleBackColor = true;
+            this.btnAggiungi.Click += new System.EventHandler(this.btnAggiungi_Click);
+            // 
+            // btnVisualizza
+            // 
+            this.btnVisualizza.Location = new System.Drawing.Point(116, 96);
+            this.btnVisualizza.Name = "btnVisualizza";
+            this.btnVisualizza.Size = new System.Drawing.Size(84, 49);
+            this.btnVisualizza.TabIndex = 7;
+            this.btnVisualizza.Text = "Visualizza";
+            this.btnVisualizza.UseVisualStyleBackColor = true;
+            this.btnVisualizza.Click += new System.EventHandler(this.btnVisualizza_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 155);
+            this.Controls.Add(this.btnVisualizza);
+            this.Controls.Add(this.btnAggiungi);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAutore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitolo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitolo;
+        private System.Windows.Forms.TextBox txtAutore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAggiungi;
+        private System.Windows.Forms.Button btnVisualizza;
+    }
+}
+
---
+++ 13-Dictionary/13-Dictionary/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct libro
+        {
+            public string titolo;
+            public string autore;
+        }
+        public int i = 0;
+        Dictionary<int, libro> dizionarioLibri = new Dictionary<int, libro>();
+
+        private void btnAggiungi_Click(object sender, EventArgs e)
+        {
+            libro l;
+            l.titolo = txtTitolo.Text;
+            l.autore = txtAutore.Text;
+            dizionarioLibri.Add(i, l);
+            i++;
+            MessageBox.Show("Libro aggiunto correttamente");
+        }
+
+        private void btnVisualizza_Click_1(object sender, EventArgs e)
+        {
+            foreach (int key in dizionarioLibri.Keys)
+                MessageBox.Show(Convert.ToString(key + 1), "N° libro");   // stampa di fatto dell'indice nel dictionary (+ 1 così parte da 1)
+
+            foreach (libro l in dizionarioLibri.Values)
+                MessageBox.Show(l.titolo + " " + l.autore, "Libri inseriti");  // stampa elementi
+        }
+    }
+}
