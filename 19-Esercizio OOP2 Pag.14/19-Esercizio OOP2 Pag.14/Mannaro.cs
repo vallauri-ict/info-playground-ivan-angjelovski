7b6a01c815e26a119b071b3fef55ede215e4cc52
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2020-12-03 17:02:46 +0100

    Aggiunto Esercizio 19

---
+++ 19-Esercizio OOP2 Pag.14/19-Esercizio OOP2 Pag.14/Mannaro.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace _19_Esercizio_OOP2_Pag._14
+{
+    class Mannaro : Mostro
+    {
+        new public string nome;
+
+        public Mannaro() : base()
+        {
+            this.nome = "Mannaro";
+        }
+
+        public Mannaro(string nome) : base(nome)
+        {
+            this.nome = "Mannaro" + nome;
+        }
+
+        new public void Rivela()
+        {
+            string s = nome + (char)(32);
+            MessageBox.Show(s + base.nome);
+        }
+    }
+}
