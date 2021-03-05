1f272d223c9873c63c28e1200432f81f8c14bf26
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-03-05 20:38:01 +0100

    Added Project 31

---
+++ 31-Progetto Delegate Dispense/31-Progetto Delegate Dispense/GestoreSalvataggio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Progetto_Delegate_Dispense
+{
+    // DICHIARO il mio delegate
+    public delegate void salvataggioEseguitoEventHandler(int id);
+    
+    class GestoreSalvataggio
+    {
+        // DICHIARO l'evento
+        public event salvataggioEseguitoEventHandler salvataggioEseguito;
+
+        public void SalvaSuFile(int id)
+        {
+            System.Windows.Forms.MessageBox.Show("Sto eseguendo il salvataggio...");
+
+            salvataggioEseguito(id);
+        }
+    }
+}
