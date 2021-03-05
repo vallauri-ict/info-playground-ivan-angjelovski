26e792fe98b4622714ba13ad4c9993d558624485
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-03-05 08:00:16 +0100

    Added Project 29

---
+++ 29-Interfaccia IComparer/29-Interfaccia IComparer/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _29_Interfaccia_IComparer
+{
+    public partial class Form1 : Form
+    {
+        Persona[] vect = {new Persona("Luca","Ariaudo","Fossano"),
+                          new Persona("Samuele","De Toffol","Bra"),
+                          new Persona("Andrea","Abbate","Alba"),
+                          new Persona("Christian","Galloro","Fossano")};
+        public Form1()
+        {
+            InitializeComponent();
+            ShowPersona();
+        }
+        public void ShowPersona()
+        {
+            string s = "";
+            foreach (var item in vect)
+            {
+                s += item.ToString() + "\n";
+            }
+            MessageBox.Show(s);
+        }
+
+        private void btnCognome_Click(object sender, EventArgs e)
+        {
+            Array.Sort(vect, new Persona.ConfrontaCognome());
+            ShowPersona();
+        }
+
+        private void btnNome_Click(object sender, EventArgs e)
+        {
+            Array.Sort(vect, new Persona.ConfrontaNome());
+            ShowPersona();
+        }
+    }
+}
